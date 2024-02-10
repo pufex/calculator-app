8f8bf5b7eba7de4bb5c6e6dfc203fd00769e5f84
pufex <j.abram1@wp.pl>
2024-02-10 17:32:42 +0100

    changes

---
+++ app.js
@@ -0,0 +1,73 @@
+const theme = document.querySelector(".theme-slider");
+const themes = [
+    ["hsl(222, 26%, 31%)", "hsl(223, 31%, 20%)", "hsl(224, 36%, 15%)", "hsl(225, 21%, 49%)", "hsl(224, 28%, 35%)", "hsl(6, 63%, 50%)", "hsl(6, 70%, 34%)", "hsl(30, 25%, 89%)", "hsl(28, 16%, 65%)", "hsl(221, 14%, 31%)"],
+    ["hsl(0, 0%, 90%)", "hsl(0, 5%, 81%)", "hsl(0, 0%, 93%)", "hsl(185, 42%, 37%)", "hsl(185, 58%, 25%)", "hsl(25, 98%, 40%)", "hsl(25, 99%, 27%)", "hsl(45, 7%, 89%)", "hsl(35, 11%, 61%)", "hsl(60, 10%, 19%)"],
+    [" hsl(281, 89%, 26%)", "hsl(285, 91%, 52%)", "hsl(176, 100%, 44%)", "hsl(177, 92%, 70%)", "hsl(268, 47%, 21%)", "hsl(290, 70%, 36%)", "hsl(52, 100%, 62%)", "hsl(198, 20%, 13%)",],
+]
+
+
+theme.addEventListener('change', () => {
+    document.documentElement.style.setProperty('--main-background', themes[theme.value][0]);
+    document.documentElement.style.setProperty('--toggle-background', themes[theme.value][1]);
+    document.documentElement.style.setProperty('--screen-background', themes[theme.value][2]);
+    document.documentElement.style.setProperty('--key-background-1', themes[theme.value][3]);
+    document.documentElement.style.setProperty('--key-shadow-1', themes[theme.value][4]);
+    document.documentElement.style.setProperty('--key-background-2', themes[theme.value][5]);
+    document.documentElement.style.setProperty('--key-shadow-2', themes[theme.value][6]);
+    document.documentElement.style.setProperty('--key-background-3', themes[theme.value][7]);
+    document.documentElement.style.setProperty('--key-shadow-3', themes[theme.value][8]);
+    document.documentElement.style.setProperty('--text', themes[themes[theme.value][9]]);
+})
+
+let calculation = "", isFunction = true;
+const numbers = document.querySelectorAll(".number");
+const functions = document.querySelectorAll(".function");
+console.log(functions);
+const reset = document.querySelector(".reset");
+const submit = document.querySelector(".submit");
+
+const displayCalculation = (currentCalculation) => {
+    const result = document.querySelector("#result");
+    result.innerText = currentCalculation;
+}
+
+numbers.forEach((number) => {
+    number.addEventListener("click", () => {
+        console.log(number.innerText);
+        calculation += number.innerText;
+        isFunction = false;
+        console.log(isFunction);
+        displayCalculation(calculation);
+    })
+})
+
+functions.forEach((item) => {
+    item.addEventListener("click", () => {
+        if(isFunction == true) return;
+        console.log("yes");
+        console.log(item.innerText);
+        switch (item.innerText){
+            case "+": calculation += "+"; break; 
+            case "-": calculation += "-"; break; 
+            case "x": calculation += "*"; break; 
+            case "/": calculation += "/"; break; 
+            default: return;
+        }
+        isFunction = true;
+        displayCalculation(calculation);
+    })
+})
+
+
+reset.addEventListener("click", () => {
+    calculation = ""
+    lastAction = "number";
+    displayCalculation(calculation);
+})
+
+submit.addEventListener("click", () => {
+    calculation = eval(calculation);
+    displayCalculation(calculation);
+    calculation = "";
+})
+
